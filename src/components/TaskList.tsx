755226772dd0352887dd5824bb7161252388f781
paulodiegooliveira <paulodiego.oliveira@gmail.com>
2022-04-05 00:52:00 -0300

    First commit

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;    
+    
+
+    const newTasks = {
+      id: Math.random(),
+      title: newTaskTitle, 
+      isComplete: false     
+    };
+
+    setTasks([...tasks, newTasks]);
+    setNewTaskTitle('');
+    
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+     
+    const checkTask = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    }: task );
+    
+    setTasks(checkTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const currentTask = tasks.filter( taskRemove => taskRemove.id !== id)    
+    setTasks(currentTask);
   }
 
   return (
